1853fe93465905140ce017b3161ae51dc5a14508
macroing <joergen.lundgren@macroing.org>
2020-08-27 23:14:33 +0200

    Add Javadocs.

--- src/main/java/org/dayflower/geometry/Point3F.java
+++ src/main/java/org/dayflower/geometry/Point3F.java
@@ -25,7 +25,14 @@ import static org.dayflower.util.Floats.min;
 import java.lang.reflect.Field;
 import java.util.Objects;
 
-//TODO: Add Javadocs!
+/**
+ * A {@code Point3F} denotes a 3-dimensional point with three coordinates, of type {@code float}.
+ * <p>
+ * This class is immutable and therefore thread-safe.
+ * 
+ * @since 1.0.0
+ * @author J&#246;rgen Lundgren
+ */
 public final class Point3F {
 	private final float component1;
 	private final float component2;
@@ -33,17 +40,46 @@ public final class Point3F {
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Constructs a new {@code Point3F} instance given the component values {@code 0.0F}, {@code 0.0F} and {@code 0.0F}.
+	 * <p>
+	 * Calling this constructor is equivalent to the following:
+	 * <pre>
+	 * {@code
+	 * new Point3F(0.0F, 0.0F, 0.0F);
+	 * }
+	 * </pre>
+	 */
 	public Point3F() {
 		this(0.0F, 0.0F, 0.0F);
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Constructs a new {@code Point3F} instance given the component values {@code v.getComponent1()}, {@code v.getComponent2()} and {@code v.getComponent3()}.
+	 * <p>
+	 * If {@code v} is {@code null}, a {@code NullPointerException} will be thrown.
+	 * <p>
+	 * Calling this constructor is equivalent to the following:
+	 * <pre>
+	 * {@code
+	 * new Point3F(v.getComponent1(), v.getComponent2(), v.getComponent3());
+	 * }
+	 * </pre>
+	 * 
+	 * @param v a {@link Vector3F} instance
+	 * @throws NullPointerException thrown if, and only if, {@code v} is {@code null}
+	 */
 	public Point3F(final Vector3F v) {
 		this(v.getComponent1(), v.getComponent2(), v.getComponent3());
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Constructs a new {@code Point3F} instance given the component values {@code component1}, {@code component2} and {@code component3}.
+	 * 
+	 * @param component1 the value of component 1
+	 * @param component2 the value of component 2
+	 * @param component3 the value of component 3
+	 */
 	public Point3F(final float component1, final float component2, final float component3) {
 		this.component1 = component1;
 		this.component2 = component2;
@@ -52,13 +88,24 @@ public final class Point3F {
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns a {@code String} representation of this {@code Point3F} instance.
+	 * 
+	 * @return a {@code String} representation of this {@code Point3F} instance
+	 */
 	@Override
 	public String toString() {
 		return String.format("new Point3F(%+.10f, %+.10f, %+.10f)", Float.valueOf(this.component1), Float.valueOf(this.component2), Float.valueOf(this.component3));
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Compares {@code object} to this {@code Point3F} instance for equality.
+	 * <p>
+	 * Returns {@code true} if, and only if, {@code object} is an instance of {@code Point3F}, and their respective values are equal, {@code false} otherwise.
+	 * 
+	 * @param object the {@code Object} to compare to this {@code Point3F} instance for equality
+	 * @return {@code true} if, and only if, {@code object} is an instance of {@code Point3F}, and their respective values are equal, {@code false} otherwise
+	 */
 	@Override
 	public boolean equals(final Object object) {
 		if(object == this) {
@@ -76,52 +123,92 @@ public final class Point3F {
 		}
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the value of component 1.
+	 * 
+	 * @return the value of component 1
+	 */
 	public float getComponent1() {
 		return this.component1;
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the value of component 2.
+	 * 
+	 * @return the value of component 2
+	 */
 	public float getComponent2() {
 		return this.component2;
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the value of component 3.
+	 * 
+	 * @return the value of component 3
+	 */
 	public float getComponent3() {
 		return this.component3;
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the value of the U-component.
+	 * 
+	 * @return the value of the U-component
+	 */
 	public float getU() {
 		return this.component1;
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the value of the V-component.
+	 * 
+	 * @return the value of the V-component
+	 */
 	public float getV() {
 		return this.component2;
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the value of the W-component.
+	 * 
+	 * @return the value of the W-component
+	 */
 	public float getW() {
 		return this.component3;
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the value of the X-component.
+	 * 
+	 * @return the value of the X-component
+	 */
 	public float getX() {
 		return this.component1;
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the value of the Y-component.
+	 * 
+	 * @return the value of the Y-component
+	 */
 	public float getY() {
 		return this.component2;
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the value of the Z-component.
+	 * 
+	 * @return the value of the Z-component
+	 */
 	public float getZ() {
 		return this.component3;
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns a hash code for this {@code Point3F} instance.
+	 * 
+	 * @return a hash code for this {@code Point3F} instance
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(Float.valueOf(this.component1), Float.valueOf(this.component2), Float.valueOf(this.component3));
@@ -129,7 +216,18 @@ public final class Point3F {
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Adds the component values of {@code vRHS} to the component values of {@code pLHS}.
+	 * <p>
+	 * Returns a new {@code Point3F} instance with the result of the addition.
+	 * <p>
+	 * If either {@code pLHS} or {@code vRHS} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param pLHS the {@code Point3F} instance on the left-hand side
+	 * @param vRHS the {@link Vector3F} instance on the right-hand side
+	 * @return a new {@code Point3F} instance with the result of the addition
+	 * @throws NullPointerException thrown if, and only if, either {@code pLHS} or {@code vRHS} are {@code null}
+	 */
 	public static Point3F add(final Point3F pLHS, final Vector3F vRHS) {
 		final float component1 = pLHS.component1 + vRHS.getComponent1();
 		final float component2 = pLHS.component2 + vRHS.getComponent2();
@@ -210,7 +308,18 @@ public final class Point3F {
 		return new Point3F(component1, component2, component3);
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Subtracts the component values of {@code vRHS} from the component values of {@code pLHS}.
+	 * <p>
+	 * Returns a new {@code Point3F} instance with the result of the subtraction.
+	 * <p>
+	 * If either {@code pLHS} or {@code vRHS} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param pLHS the {@code Point3F} instance on the left-hand side
+	 * @param vRHS the {@link Vector3F} instance on the right-hand side
+	 * @return a new {@code Point3F} instance with the result of the subtraction
+	 * @throws NullPointerException thrown if, and only if, either {@code pLHS} or {@code vRHS} are {@code null}
+	 */
 	public static Point3F subtract(final Point3F pLHS, final Vector3F vRHS) {
 		final float component1 = pLHS.component1 - vRHS.getComponent1();
 		final float component2 = pLHS.component2 - vRHS.getComponent2();
@@ -238,12 +347,30 @@ public final class Point3F {
 		return equal(component4, 1.0F) || equal(component4, 0.0F) ? new Point3F(component1, component2, component3) : new Point3F(component1 / component4, component2 / component4, component3 / component4);
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the distance from {@code pEye} to {@code pLookAt}.
+	 * <p>
+	 * If either {@code pEye} or {@code pLookAt} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param pEye a {@code Point3F} instance
+	 * @param pLookAt a {@code Point3F} instance
+	 * @return the distance from {@code pEye} to {@code pLookAt}
+	 * @throws NullPointerException thrown if, and only if, either {@code pEye} or {@code pLookAt} are {@code null}
+	 */
 	public static float distance(final Point3F pEye, final Point3F pLookAt) {
 		return Vector3F.direction(pEye, pLookAt).length();
 	}
 	
-//	TODO: Add Javadocs!
+	/**
+	 * Returns the squared distance from {@code pEye} to {@code pLookAt}.
+	 * <p>
+	 * If either {@code pEye} or {@code pLookAt} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param pEye a {@code Point3F} instance
+	 * @param pLookAt a {@code Point3F} instance
+	 * @return the squared distance from {@code pEye} to {@code pLookAt}
+	 * @throws NullPointerException thrown if, and only if, either {@code pEye} or {@code pLookAt} are {@code null}
+	 */
 	public static float distanceSquared(final Point3F pEye, final Point3F pLookAt) {
 		return Vector3F.direction(pEye, pLookAt).lengthSquared();
 	}

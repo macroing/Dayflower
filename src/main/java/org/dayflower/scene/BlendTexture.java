db47a0f8f2991ec0a69458d62b7b0b8ec0d74dd7
macroing <joergen.lundgren@macroing.org>
2020-09-03 01:01:37 +0200

    Update the Scene API.

--- src/main/java/org/dayflower/scene/BlendTexture.java
+++ src/main/java/org/dayflower/scene/BlendTexture.java
@@ -18,16 +18,159 @@
  */
 package org.dayflower.scene;
 
-import java.lang.reflect.Field;
+import static org.dayflower.util.Floats.equal;
+
+import java.util.Objects;
 
 import org.dayflower.geometry.SurfaceIntersection3F;
 import org.dayflower.image.Color3F;
 
-//TODO: Add Javadocs!
+/**
+ * A {@code BlendTexture} is a {@link Texture} implementation that returns a {@link Color3F} instance by blending the result of two other {@code Texture} instances.
+ * <p>
+ * This class is immutable and therefore thread-safe.
+ * 
+ * @since 1.0.0
+ * @author J&#246;rgen Lundgren
+ */
 public final class BlendTexture implements Texture {
-//	TODO: Add Javadocs!
+	private final Texture textureA;
+	private final Texture textureB;
+	private final float tComponent1;
+	private final float tComponent2;
+	private final float tComponent3;
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Constucts a new {@code BlendTexture} instance.
+	 * <p>
+	 * If either {@code textureA} or {@code textureB} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * <p>
+	 * Calling this constructor is equivalent to the following:
+	 * <pre>
+	 * {@code
+	 * new BlendTexture(textureA, textureB, 0.5F);
+	 * }
+	 * </pre>
+	 * 
+	 * @param textureA one of the two {@link Texture} instances to blend
+	 * @param textureB one of the two {@code Texture} instances to blend
+	 * @throws NullPointerException thrown if, and only if, either {@code textureA} or {@code textureB} are {@code null}
+	 */
+	public BlendTexture(final Texture textureA, final Texture textureB) {
+		this(textureA, textureB, 0.5F);
+	}
+	
+	/**
+	 * Constucts a new {@code BlendTexture} instance.
+	 * <p>
+	 * If either {@code textureA} or {@code textureB} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * <p>
+	 * Calling this constructor is equivalent to the following:
+	 * <pre>
+	 * {@code
+	 * new BlendTexture(textureA, textureB, t, t, t);
+	 * }
+	 * </pre>
+	 * 
+	 * @param textureA one of the two {@link Texture} instances to blend
+	 * @param textureB one of the two {@code Texture} instances to blend
+	 * @param t the factor to use for all components in the blending process
+	 * @throws NullPointerException thrown if, and only if, either {@code textureA} or {@code textureB} are {@code null}
+	 */
+	public BlendTexture(final Texture textureA, final Texture textureB, final float t) {
+		this(textureA, textureB, t, t, t);
+	}
+	
+	/**
+	 * Constucts a new {@code BlendTexture} instance.
+	 * <p>
+	 * If either {@code textureA} or {@code textureB} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param textureA one of the two {@link Texture} instances to blend
+	 * @param textureB one of the two {@code Texture} instances to blend
+	 * @param tComponent1 the factor to use for component 1 in the blending process
+	 * @param tComponent2 the factor to use for component 2 in the blending process
+	 * @param tComponent3 the factor to use for component 3 in the blending process
+	 * @throws NullPointerException thrown if, and only if, either {@code textureA} or {@code textureB} are {@code null}
+	 */
+	public BlendTexture(final Texture textureA, final Texture textureB, final float tComponent1, final float tComponent2, final float tComponent3) {
+		this.textureA = Objects.requireNonNull(textureA, "textureA == null");
+		this.textureB = Objects.requireNonNull(textureB, "textureB == null");
+		this.tComponent1 = tComponent1;
+		this.tComponent2 = tComponent2;
+		this.tComponent3 = tComponent3;
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Returns a {@link Color3F} instance representing the color of the surface at {@code surfaceIntersection}.
+	 * <p>
+	 * If {@code surfaceIntersection} is {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param surfaceIntersection a {@link SurfaceIntersection3F} instance
+	 * @return a {@code Color3F} instance representing the color of the surface at {@code surfaceIntersection}
+	 * @throws NullPointerException thrown if, and only if, {@code surfaceIntersection} is {@code null}
+	 */
 	@Override
 	public Color3F getColor(final SurfaceIntersection3F surfaceIntersection) {
-		return Color3F.BLACK;//TODO: Implement!
+		Objects.requireNonNull(surfaceIntersection, "surfaceIntersection == null");
+		
+		final Color3F colorA = this.textureA.getColor(surfaceIntersection);
+		final Color3F colorB = this.textureB.getColor(surfaceIntersection);
+		final Color3F colorC = Color3F.blend(colorA, colorB, this.tComponent1, this.tComponent2, this.tComponent3);
+		
+		return colorC;
+	}
+	
+	/**
+	 * Returns a {@code String} representation of this {@code BlendTexture} instance.
+	 * 
+	 * @return a {@code String} representation of this {@code BlendTexture} instance
+	 */
+	@Override
+	public String toString() {
+		return String.format("new BlendTexture(%s, %s, %+.10f, %+.10f, %+.10f)", this.textureA, this.textureB, Float.valueOf(this.tComponent1), Float.valueOf(this.tComponent2), Float.valueOf(this.tComponent3));
+	}
+	
+	/**
+	 * Compares {@code object} to this {@code BlendTexture} instance for equality.
+	 * <p>
+	 * Returns {@code true} if, and only if, {@code object} is an instance of {@code BlendTexture}, and their respective values are equal, {@code false} otherwise.
+	 * 
+	 * @param object the {@code Object} to compare to this {@code BlendTexture} instance for equality
+	 * @return {@code true} if, and only if, {@code object} is an instance of {@code BlendTexture}, and their respective values are equal, {@code false} otherwise
+	 */
+	@Override
+	public boolean equals(final Object object) {
+		if(object == this) {
+			return true;
+		} else if(!(object instanceof BlendTexture)) {
+			return false;
+		} else if(!Objects.equals(this.textureA, BlendTexture.class.cast(object).textureA)) {
+			return false;
+		} else if(!Objects.equals(this.textureB, BlendTexture.class.cast(object).textureB)) {
+			return false;
+		} else if(!equal(this.tComponent1, BlendTexture.class.cast(object).tComponent1)) {
+			return false;
+		} else if(!equal(this.tComponent2, BlendTexture.class.cast(object).tComponent2)) {
+			return false;
+		} else if(!equal(this.tComponent3, BlendTexture.class.cast(object).tComponent3)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	/**
+	 * Returns a hash code for this {@code BlendTexture} instance.
+	 * 
+	 * @return a hash code for this {@code BlendTexture} instance
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.textureA, this.textureB, Float.valueOf(this.tComponent1), Float.valueOf(this.tComponent2), Float.valueOf(this.tComponent3));
 	}
 }
--- src/main/java/org/dayflower/scene/BullseyeTexture.java
+++ src/main/java/org/dayflower/scene/BullseyeTexture.java
@@ -18,16 +18,137 @@
  */
 package org.dayflower.scene;
 
-import java.lang.reflect.Field;
+import static org.dayflower.util.Floats.equal;
+import static org.dayflower.util.Floats.remainder;
 
+import java.util.Objects;
+
+import org.dayflower.geometry.Point3F;
 import org.dayflower.geometry.SurfaceIntersection3F;
+import org.dayflower.geometry.Vector3F;
 import org.dayflower.image.Color3F;
 
-//TODO: Add Javadocs!
+/**
+ * A {@code BullseyeTexture} is a {@link Texture} implementation that returns a {@link Color3F} instance by alternating between two other {@code Texture} instances in a bullseye pattern.
+ * <p>
+ * This class is immutable and therefore thread-safe.
+ * 
+ * @since 1.0.0
+ * @author J&#246;rgen Lundgren
+ */
 public final class BullseyeTexture implements Texture {
-//	TODO: Add Javadocs!
+	private final Point3F origin;
+	private final Texture textureA;
+	private final Texture textureB;
+	private final float scale;
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Constructs a new {@code BullseyeTexture} instance.
+	 * <p>
+	 * If either {@code origin}, {@code textureA} or {@code textureB} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * <p>
+	 * Calling this constructor is equivalent to the following:
+	 * <pre>
+	 * {@code
+	 * new BullseyeTexture(origin, textureA, textureB, 1.0F);
+	 * }
+	 * </pre>
+	 * 
+	 * @param origin a {@link Point3F} instance used as the origin for the bullseye pattern
+	 * @param textureA one of the two {@link Texture} instances to use
+	 * @param textureB one of the two {@code Texture} instances to use
+	 * @throws NullPointerException thrown if, and only if, either {@code origin}, {@code textureA} or {@code textureB} are {@code null}
+	 */
+	public BullseyeTexture(final Point3F origin, final Texture textureA, final Texture textureB) {
+		this(origin, textureA, textureB, 1.0F);
+	}
+	
+	/**
+	 * Constructs a new {@code BullseyeTexture} instance.
+	 * <p>
+	 * If either {@code origin}, {@code textureA} or {@code textureB} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param origin a {@link Point3F} instance used as the origin for the bullseye pattern
+	 * @param textureA one of the two {@link Texture} instances to use
+	 * @param textureB one of the two {@code Texture} instances to use
+	 * @param scale the scale for the bullseye pattern
+	 * @throws NullPointerException thrown if, and only if, either {@code origin}, {@code textureA} or {@code textureB} are {@code null}
+	 */
+	public BullseyeTexture(final Point3F origin, final Texture textureA, final Texture textureB, final float scale) {
+		this.origin = Objects.requireNonNull(origin, "origin == null");
+		this.textureA = Objects.requireNonNull(textureA, "textureA == null");
+		this.textureB = Objects.requireNonNull(textureB, "textureB == null");
+		this.scale = scale;
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Returns a {@link Color3F} instance representing the color of the surface at {@code surfaceIntersection}.
+	 * <p>
+	 * If {@code surfaceIntersection} is {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param surfaceIntersection a {@link SurfaceIntersection3F} instance
+	 * @return a {@code Color3F} instance representing the color of the surface at {@code surfaceIntersection}
+	 * @throws NullPointerException thrown if, and only if, {@code surfaceIntersection} is {@code null}
+	 */
 	@Override
 	public Color3F getColor(final SurfaceIntersection3F surfaceIntersection) {
-		return Color3F.BLACK;//TODO: Implement!
+		final Vector3F direction = Vector3F.direction(this.origin, surfaceIntersection.getSurfaceIntersectionPoint());
+		
+		final float directionLength = direction.length();
+		final float directionLengthScaled = directionLength * this.scale;
+		final float directionLengthScaledRemainder = remainder(directionLengthScaled, 1.0F);
+		
+		return directionLengthScaledRemainder > 0.5F ? this.textureA.getColor(surfaceIntersection) : this.textureB.getColor(surfaceIntersection);
+	}
+	
+	/**
+	 * Returns a {@code String} representation of this {@code BullseyeTexture} instance.
+	 * 
+	 * @return a {@code String} representation of this {@code BullseyeTexture} instance
+	 */
+	@Override
+	public String toString() {
+		return String.format("new BullseyeTexture(%s, %s, %s, %+.10f)", this.origin, this.textureA, this.textureB, Float.valueOf(this.scale));
+	}
+	
+	/**
+	 * Compares {@code object} to this {@code BullseyeTexture} instance for equality.
+	 * <p>
+	 * Returns {@code true} if, and only if, {@code object} is an instance of {@code BullseyeTexture}, and their respective values are equal, {@code false} otherwise.
+	 * 
+	 * @param object the {@code Object} to compare to this {@code BullseyeTexture} instance for equality
+	 * @return {@code true} if, and only if, {@code object} is an instance of {@code BullseyeTexture}, and their respective values are equal, {@code false} otherwise
+	 */
+	@Override
+	public boolean equals(final Object object) {
+		if(object == this) {
+			return true;
+		} else if(!(object instanceof BullseyeTexture)) {
+			return false;
+		} else if(!Objects.equals(this.origin, BullseyeTexture.class.cast(object).origin)) {
+			return false;
+		} else if(!Objects.equals(this.textureA, BullseyeTexture.class.cast(object).textureA)) {
+			return false;
+		} else if(!Objects.equals(this.textureB, BullseyeTexture.class.cast(object).textureB)) {
+			return false;
+		} else if(!equal(this.scale, BullseyeTexture.class.cast(object).scale)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	/**
+	 * Returns a hash code for this {@code BullseyeTexture} instance.
+	 * 
+	 * @return a hash code for this {@code BullseyeTexture} instance
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.origin, this.textureA, this.textureB, Float.valueOf(this.scale));
 	}
 }

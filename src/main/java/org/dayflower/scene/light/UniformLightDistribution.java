3f43e0be37b45b5b8a725def7508bc916aabf86d
macroing <joergen.lundgren@macroing.org>
2020-11-26 07:57:45 +0100

    Update the Scene API.

--- src/main/java/org/dayflower/scene/light/UniformLightDistribution.java
+++ src/main/java/org/dayflower/scene/light/UniformLightDistribution.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Dayflower. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.dayflower.scene.background;
+package org.dayflower.scene.light;
 
 import static org.dayflower.util.Floats.array;
 

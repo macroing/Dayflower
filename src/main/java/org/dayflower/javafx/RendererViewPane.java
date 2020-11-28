bdc377bc48314566f8392064ee28e179337a4a70
macroing <joergen.lundgren@macroing.org>
2020-11-28 09:21:46 +0100

    Update the JavaFX API.

--- src/main/java/org/dayflower/javafx/RendererViewPane.java
+++ src/main/java/org/dayflower/javafx/RendererViewPane.java
@@ -74,6 +74,15 @@ final class RendererViewPane extends BorderPane {
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	
+	/**
+	 * Constructs a new {@code RendererViewPane} instance.
+	 * <p>
+	 * If either {@code renderer} or {@code executorService} are {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param renderer the {@link Renderer} instance associated with this {@code RendererViewPane} instance
+	 * @param executorService the {@code ExecutorService} associated with this {@code RendererViewPane} instance
+	 * @throws NullPointerException thrown if, and only if, either {@code renderer} or {@code executorService} are {@code null}
+	 */
 	public RendererViewPane(final Renderer renderer, final ExecutorService executorService) {
 		this.keysPressed = new AtomicInteger();
 		this.mouseButtonsPressed = new AtomicInteger();
@@ -101,14 +110,162 @@ final class RendererViewPane extends BorderPane {
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	
+	/**
+	 * Returns the {@code ExecutorService} instance associated with this {@code RendererViewPane} instance.
+	 * 
+	 * @return the {@code ExecutorService} instance associated with this {@code RendererViewPane} instance
+	 */
+	public ExecutorService getExecutorService() {
+		return this.executorService;
+	}
+	
+	/**
+	 * Returns the optional {@code File} for saving.
+	 * 
+	 * @return the optional {@code File} for saving
+	 */
 	public Optional<File> getFile() {
 		return Optional.ofNullable(this.file.get());
 	}
 	
+	/**
+	 * Returns the {@link Renderer} instance associated with this {@code RendererViewPane} instance.
+	 * 
+	 * @return the {@code Renderer} instance associated with this {@code RendererViewPane} instance
+	 */
 	public Renderer getRenderer() {
 		return this.renderer;
 	}
 	
+	/**
+	 * Returns {@code true} if, and only if, at least one key is being pressed, {@code false} otherwise.
+	 * 
+	 * @return {@code true} if, and only if, at least one key is being pressed, {@code false} otherwise
+	 */
+	public boolean isKeyPressed() {
+		return this.keysPressed.get() > 0;
+	}
+	
+	/**
+	 * Returns {@code true} if, and only if, the key denoted by {@code keyCode} is being pressed, {@code false} otherwise.
+	 * <p>
+	 * Calling this method is equivalent to calling {@code isKeyPressed(keyCode, false)}.
+	 * <p>
+	 * If {@code keyCode} is {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param keyCode a {@code KeyCode}
+	 * @return {@code true} if, and only if, the key denoted by {@code keyCode} is being pressed, {@code false} otherwise
+	 * @throws NullPointerException thrown if, and only if, {@code keyCode} is {@code null}
+	 */
+	public boolean isKeyPressed(final KeyCode keyCode) {
+		return isKeyPressed(keyCode, false);
+	}
+	
+	/**
+	 * Returns {@code true} if, and only if, the key denoted by {@code keyCode} is being pressed, {@code false} otherwise.
+	 * <p>
+	 * If {@code isKeyPressedOnce} is {@code true}, only the first call to this method will return {@code true} per press-release cycle given a specific key.
+	 * <p>
+	 * If {@code keyCode} is {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param keyCode a {@code KeyCode}
+	 * @param isKeyPressedOnce {@code true} if, and only if, a key press should occur at most one time per press-release cycle, {@code false} otherwise
+	 * @return {@code true} if, and only if, the key denoted by {@code keyCode} is being pressed, {@code false} otherwise
+	 * @throws NullPointerException thrown if, and only if, {@code keyCode} is {@code null}
+	 */
+	public boolean isKeyPressed(final KeyCode keyCode, final boolean isKeyPressedOnce) {
+		final boolean isKeyPressed = this.isKeyPressed[keyCode.ordinal()];
+		
+		if(isKeyPressedOnce) {
+			final boolean isKeyPressedOnce0 = this.isKeyPressedOnce[keyCode.ordinal()];
+			
+			if(isKeyPressed && !isKeyPressedOnce0) {
+				this.isKeyPressedOnce[keyCode.ordinal()] = true;
+				
+				return true;
+			}
+			
+			return false;
+		}
+		
+		return isKeyPressed;
+	}
+	
+	/**
+	 * Returns {@code true} if, and only if, at least one mouse button is being pressed, {@code false} otherwise.
+	 * 
+	 * @return {@code true} if, and only if, at least one mouse button is being pressed, {@code false} otherwise
+	 */
+	public boolean isMouseButtonPressed() {
+		return this.mouseButtonsPressed.get() > 0;
+	}
+	
+	/**
+	 * Returns {@code true} if, and only if, the mouse button denoted by {@code mouseButton} is being pressed, {@code false} otherwise.
+	 * <p>
+	 * Calling this method is equivalent to calling {@code isMouseButtonPressed(mouseButton, false)}.
+	 * <p>
+	 * If {@code mouseButton} is {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param mouseButton a {@code MouseButton}
+	 * @return {@code true} if, and only if, the mouse button denoted by {@code mouseButton} is being pressed, {@code false} otherwise
+	 * @throws NullPointerException thrown if, and only if, {@code mouseButton} is {@code null}
+	 */
+	public boolean isMouseButtonPressed(final MouseButton mouseButton) {
+		return isMouseButtonPressed(mouseButton, false);
+	}
+	
+	/**
+	 * Returns {@code true} if, and only if, the mouse button denoted by {@code mouseButton} is being pressed, {@code false} otherwise.
+	 * <p>
+	 * If {@code isMouseButtonPressedOnce} is {@code true}, only the first call to this method will return {@code true} per press-release cycle given a specific mouse button.
+	 * <p>
+	 * If {@code mouseButton} is {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param mouseButton a {@code MouseButton}
+	 * @param isMouseButtonPressedOnce {@code true} if, and only if, a mouse button press should occur at most one time per press-release cycle, {@code false} otherwise
+	 * @return {@code true} if, and only if, the mouse button denoted by {@code mouseButton} is being pressed, {@code false} otherwise
+	 * @throws NullPointerException thrown if, and only if, {@code mouseButton} is {@code null}
+	 */
+	public boolean isMouseButtonPressed(final MouseButton mouseButton, final boolean isMouseButtonPressedOnce) {
+		final boolean isMouseButtonPressed = this.isMouseButtonPressed[mouseButton.ordinal()];
+		
+		if(isMouseButtonPressedOnce) {
+			final boolean isMouseButtonPressedOnce0 = this.isMouseButtonPressedOnce[mouseButton.ordinal()];
+			
+			if(isMouseButtonPressed && !isMouseButtonPressedOnce0) {
+				this.isMouseButtonPressedOnce[mouseButton.ordinal()] = true;
+				
+				return true;
+			}
+			
+			return false;
+		}
+		
+		return isMouseButtonPressed;
+	}
+	
+	/**
+	 * Returns the X-coordinate of the mouse.
+	 * 
+	 * @return the X-coordinate of the mouse
+	 */
+	public double getMouseX() {
+		return Double.longBitsToDouble(this.mouseX.get());
+	}
+	
+	/**
+	 * Returns the Y-coordinate of the mouse.
+	 * 
+	 * @return the Y-coordinate of the mouse
+	 */
+	public double getMouseY() {
+		return Double.longBitsToDouble(this.mouseY.get());
+	}
+	
+	/**
+	 * This method is called when it's time to render.
+	 */
 	public void render() {
 		final RendererTask oldRendererTask = this.rendererTask.get();
 		
@@ -146,6 +303,9 @@ final class RendererViewPane extends BorderPane {
 		}
 	}
 	
+	/**
+	 * This method is called when it's time to save the {@link Image}.
+	 */
 	public void save() {
 		final Optional<File> optionalFile = getFile();
 		
@@ -162,6 +322,14 @@ final class RendererViewPane extends BorderPane {
 		}
 	}
 	
+	/**
+	 * Sets the {@code File} for saving to {@code file}.
+	 * <p>
+	 * If {@code file} is {@code null}, a {@code NullPointerException} will be thrown.
+	 * 
+	 * @param file the {@code File} for saving
+	 * @throws NullPointerException} thrown if, and only if, {@code file} is {@code null}
+	 */
 	public void setFile(final File file) {
 		this.file.set(Objects.requireNonNull(file, "file == null"));
 	}

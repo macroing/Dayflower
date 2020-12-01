801f18865d1fdb4dde8ad7020af0162ec49a1a05
macroing <joergen.lundgren@macroing.org>
2020-12-01 16:28:03 +0100

    Update scenes.

--- resources/scenes/RayitoShowcaseMaterialReflectionMaterial.java
+++ resources/scenes/RayitoShowcaseMaterialReflectionMaterial.java
@@ -4,16 +4,11 @@ Material material2 = new ReflectionMaterial();
 Shape3F shape1 = new Plane3F();
 Shape3F shape2 = new Sphere3F(10.0F);
 
-Texture texture11 = new ConstantTexture(Color3F.WHITE);
-Texture texture12 = new ConstantTexture();
-Texture texture21 = new ConstantTexture(Color3F.GRAY);
-Texture texture22 = new ConstantTexture();
-
-Matrix44F matrix1 = Matrix44F.identity();
-Matrix44F matrix2 = Matrix44F.translate(0.0F, 2.0F, 20.0F);
+Transform transform1 = new Transform(new Point3F(0.0F, 0.0F,  0.0F));
+Transform transform2 = new Transform(new Point3F(0.0F, 2.0F, 30.0F));
 
 scene.addLight(new PerezLight());
-scene.addPrimitive(new Primitive(material1, shape1, texture11, texture12, matrix1));
-scene.addPrimitive(new Primitive(material2, shape2, texture21, texture22, matrix2));
+scene.addPrimitive(new Primitive(material1, shape1, transform1));
+scene.addPrimitive(new Primitive(material2, shape2, transform2));
 scene.setCamera(new Camera(new Point3F(0.0F, 2.0F, 0.0F)));
 scene.setName("RayitoShowcaseMaterialReflectionMaterial");

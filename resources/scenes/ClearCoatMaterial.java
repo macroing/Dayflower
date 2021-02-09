8498de0384b11a5d0212b40742abe8817d9019bc
macroing <joergen.lundgren@macroing.org>
2021-02-09 17:40:19 +0100

    Add new scenes.

--- resources/scenes/ClearCoatMaterial.java
+++ resources/scenes/ClearCoatMaterial.java
@@ -1,5 +1,5 @@
 Material material1 = new MatteMaterial(new CheckerboardTexture());
-Material material2 = new MetalMaterial();
+Material material2 = new ClearCoatMaterial(new Color3F(0.5F, 0.01F, 0.01F));
 
 Shape3F shape1 = new Plane3F();
 Shape3F shape2 = new Sphere3F();
@@ -11,4 +11,4 @@ scene.addLight(new PerezLight());
 scene.addPrimitive(new Primitive(material1, shape1, transform1));
 scene.addPrimitive(new Primitive(material2, shape2, transform2));
 scene.setCamera(new Camera(new Point3F(0.0F, 2.0F, -10.0F), AngleF.degrees(40.0F)));
-scene.setName("MetalMaterial");
+scene.setName("ClearCoatMaterial");
--- resources/scenes/HairMaterial.java
+++ resources/scenes/HairMaterial.java
@@ -1,14 +1,14 @@
 Material material1 = new MatteMaterial(new CheckerboardTexture());
-Material material2 = new MetalMaterial();
+Material material2 = new HairMaterial();
 
 Shape3F shape1 = new Plane3F();
 Shape3F shape2 = new Sphere3F();
 
 Transform transform1 = new Transform(new Point3F(0.0F, 0.0F, 0.0F));
-Transform transform2 = new Transform(new Point3F(0.0F, 1.0F, 0.0F));
+Transform transform2 = new Transform(new Point3F(0.0F, 1.0F, 0.0F), Quaternion4F.from(Matrix44F.rotateX(AngleF.degrees(270.0F))));
 
 scene.addLight(new PerezLight());
 scene.addPrimitive(new Primitive(material1, shape1, transform1));
 scene.addPrimitive(new Primitive(material2, shape2, transform2));
 scene.setCamera(new Camera(new Point3F(0.0F, 2.0F, -10.0F), AngleF.degrees(40.0F)));
-scene.setName("MetalMaterial");
+scene.setName("HairMaterial");

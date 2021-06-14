90874864045eda6f79fe94deb27e6a5e0063656c
macroing <joergen.lundgren@macroing.org>
2021-06-14 20:31:11 +0200

    Update the JavaFX API.

--- src/main/java/org/dayflower/javafx/scene/control/package-info.java
+++ src/main/java/org/dayflower/javafx/scene/control/package-info.java
@@ -1,4 +1,17 @@
 /**
  * Provides the JavaFX Scene Control API.
+ * <p>
+ * The JavaFX Scene Control API is an extension to {@code javafx.scene.control}.
+ * <h3>Overview</h3>
+ * <p>
+ * The following list contains information about the classes in this API.
+ * <ul>
+ * <li>{@link org.dayflower.javafx.scene.control.ContextMenus ContextMenus} consists exclusively of static methods that returns or performs various operations on {@code ContextMenu} instances.</li>
+ * <li>{@link org.dayflower.javafx.scene.control.Labels Labels} consists exclusively of static methods that returns or performs various operations on {@code Label} instances.</li>
+ * <li>{@link org.dayflower.javafx.scene.control.MenuItems MenuItems} consists exclusively of static methods that returns or performs various operations on {@code MenuItem} instances.</li>
+ * <li>{@link org.dayflower.javafx.scene.control.NodeSelectionTabPane NodeSelectionTabPane} is a {@code TabPane} that provides a method to retrieve the selected {@code Node} instance and simplifies {@code Tab} creation and selection.</li>
+ * <li>{@link org.dayflower.javafx.scene.control.ObjectTreeView ObjectTreeView} is a {@code TreeView} that simplifies {@code TreeItem} creation with or without {@code ContextMenu} instances.</li>
+ * <li>{@link org.dayflower.javafx.scene.control.PathMenuBar PathMenuBar} is a {@code MenuBar} that allows you to add {@code CheckMenuItem}, {@code MenuItem} and {@code SeparatorMenuItem} instances in a straight forward way.</li>
+ * </ul>
  */
 package org.dayflower.javafx.scene.control;

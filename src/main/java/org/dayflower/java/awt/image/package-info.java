83a6b267b5c22622bd186c4640397038f11ba533
macroing <joergen.lundgren@macroing.org>
2021-07-04 13:23:23 +0200

    Update the Java API.

--- src/main/java/org/dayflower/java/awt/image/package-info.java
+++ src/main/java/org/dayflower/java/awt/image/package-info.java
@@ -2,5 +2,11 @@
  * Provides the Java AWT Image API.
  * <p>
  * The Java AWT Image API is an extension to {@code java.awt.image}.
+ * <h3>Overview</h3>
+ * <p>
+ * The following list contains information about the classes in this API.
+ * <ul>
+ * <li>{@link org.dayflower.java.awt.image.BufferedImages BufferedImages} consists exclusively of static methods that returns or performs various operations on {@code BufferedImage} instances.</li>
+ * </ul>
  */
 package org.dayflower.java.awt.image;
--- src/main/java/org/dayflower/java/util/function/package-info.java
+++ src/main/java/org/dayflower/java/util/function/package-info.java
@@ -2,5 +2,14 @@
  * Provides the Java Util Function API.
  * <p>
  * The Java Util Function API is an extension to {@code java.util.function}.
+ * <h3>Overview</h3>
+ * <p>
+ * The following list contains information about the interfaces in this API.
+ * <ul>
+ * <li>{@link org.dayflower.java.util.function.FloatBinaryOperator FloatBinaryOperator} represents an operation upon two {@code float}-valued operands and producing a {@code float}-valued result.</li>
+ * <li>{@link org.dayflower.java.util.function.FloatSupplier FloatSupplier} represents a supplier of {@code float}-valued results.</li>
+ * <li>{@link org.dayflower.java.util.function.FloatUnaryOperator FloatUnaryOperator} represents an operation on a single {@code float}-valued operand that produces a {@code float}-valued result.</li>
+ * <li>{@link org.dayflower.java.util.function.TriFunction TriFunction} represents a function that accepts three arguments and produces a result.</li>
+ * </ul>
  */
 package org.dayflower.java.util.function;

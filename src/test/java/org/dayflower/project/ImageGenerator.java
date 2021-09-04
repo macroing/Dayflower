bebba0a0244d4853c356c924b9f5b3bad518f3e0
macroing <joergen.lundgren@macroing.org>
2021-09-04 11:16:55 +0200

    Add the ImageGenerator class to generate images for all scenes to the Test API.

---
+++ src/test/java/org/dayflower/project/ImageGenerator.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2014 - 2021 J&#246;rgen Lundgren
+ * 
+ * This file is part of Dayflower.
+ * 
+ * Dayflower is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Dayflower is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Dayflower. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dayflower.project;
+
+import java.io.File;
+import java.util.List;
+
+import org.dayflower.java.io.Files;
+import org.dayflower.renderer.RendererObserver;
+import org.dayflower.renderer.RenderingAlgorithm;
+import org.dayflower.renderer.gpu.GPURenderer;
+import org.dayflower.renderer.observer.FileRendererObserver;
+import org.dayflower.scene.Scene;
+import org.dayflower.scene.SceneLoader;
+import org.dayflower.scene.loader.JavaSceneLoader;
+
+public final class ImageGenerator {
+	private ImageGenerator() {
+		
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public static void main(final String[] args) {
+		final List<File> files = Files.findFilesFromDirectory(new File("./resources/scenes"));
+		
+		for(final File file : files) {
+			doRender(file);
+		}
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static void doRender(final File file) {
+		final SceneLoader sceneLoader = new JavaSceneLoader();
+		
+		final Scene scene = sceneLoader.load(file);
+		
+		System.out.println("Rendering " + scene.getName() + "...");
+		
+		final String pathname = String.format("./generated/scenes/%s.png", scene.getName());
+		
+		final RendererObserver rendererObserver = new FileRendererObserver(pathname, false, false, true, 100);
+		
+		final
+		GPURenderer gPURenderer = new GPURenderer(rendererObserver);
+		gPURenderer.setScene(scene);
+		gPURenderer.setImage();
+		gPURenderer.setMaximumBounce(20);
+		gPURenderer.setMinimumBounceRussianRoulette(5);
+		gPURenderer.setRenderingAlgorithm(RenderingAlgorithm.PATH_TRACING);
+		gPURenderer.setup();
+		
+		for(int i = 0; i < 2000; i++) {
+			gPURenderer.render();
+		}
+		
+		gPURenderer.dispose();
+		
+		System.out.println("Done.");
+	}
+}

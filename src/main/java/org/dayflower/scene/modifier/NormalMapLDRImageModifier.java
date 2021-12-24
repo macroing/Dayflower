22c9d9af14119717f956c3188b1073439f3964a6
macroing <joergen.lundgren@macroing.org>
2021-12-24 11:46:54 +0100

    Update the Scene API.

--- src/main/java/org/dayflower/scene/modifier/NormalMapLDRImageModifier.java
+++ src/main/java/org/dayflower/scene/modifier/NormalMapLDRImageModifier.java
@@ -289,6 +289,15 @@ public final class NormalMapLDRImageModifier extends AbstractLDRImageModifier {
 		
 		final Color3F colorRGB = getColorRGB(intersection.getTextureCoordinates());
 		
+//		TODO: Find out if something like the following is needed. The RGB-component value 128 should be mapped to 0, but it is not in the current approach.
+//		final int r = colorRGB.getAsIntR();
+//		final int g = colorRGB.getAsIntG();
+//		final int b = colorRGB.getAsIntB();
+		
+//		final float x = r * 2 / 255 - 1;
+//		final float y = g * 2 / 255 - 1;
+//		final float z = b * 2 / 255 - 1;
+		
 		final float r = colorRGB.getR();
 		final float g = colorRGB.getG();
 		final float b = colorRGB.getB();
